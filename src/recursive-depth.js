e232f57440a03fe95de9f7427d08a0d728d677c5
Arthur Iorbalidi <arturiorbalidi01052005@gmail.com>
2023-12-11 23:29:19 +0300

    feat: make 18 tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let depth = 1;
+    for (const element of arr) {
+      if (Array.isArray(element)) {
+        const currentdepth = this.calculateDepth(element) + 1;
+        if (currentdepth > depth) {
+          depth = currentdepth;
+        }
+      }
+    }
+    return depth;
   }
 }
 
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const sortedBesidesMinusOne = arr.filter((height) => height !== -1).sort((a, b) => a - b);
+  let i = 0;
+  let result = arr.map((elem) => {
+    if(elem === -1) {
+      return elem;
+    } else {
+      return sortedBesidesMinusOne[i++];
+    }
+  })
+  return result;
 }
 
 module.exports = {
